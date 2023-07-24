33573cdcafbf8130c76c365cc89de6264dae43bd
Fatima629 <JIAJUN36.22@ichat.sp.edu.sg>
2023-07-24 16:27:14 +0800

    Changed Node Modules

--- model/databaseConfig.js
+++ model/databaseConfig.js
@@ -4,7 +4,7 @@ var dbconnect = {
         var conn = mysql.createConnection({
             host: "localhost",
             user: "root",
-            password: "z9N3Eif7Y5",
+            password: "root",
             database: "islandfurniture-it07"
         });
         return conn;
